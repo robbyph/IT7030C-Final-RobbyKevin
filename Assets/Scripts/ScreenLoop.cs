8b0648a9ed0f72b41623416358c688fcbddf17dd
Robby <robbyphoover@gmail.com>
2024-12-09 19:40:59 -0500

    Improved parallax scrolling that accounts for going left

--- Assets/Scripts/ScreenLoop.cs
+++ Assets/Scripts/ScreenLoop.cs
@@ -15,18 +15,31 @@ public class EndlessBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lowerLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)); // Get the lower left corner of the screen
-        float bgMaxX = backgrounds[0].GetComponent<Transform>().position.x + backgrounds[0].GetComponent<SpriteRenderer>().bounds.size.x / 2; // Get the rightmost x position of the background
+        Vector3 lowerLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 lowerRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
 
-        if (lowerLeft.x > bgMaxX)
+        // Get the boundaries of the first and last background
+        float leftBGMinX = backgrounds[0].GetComponent<Transform>().position.x - backgrounds[0].GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        float rightBGMaxX = backgrounds[backgrounds.Count - 1].GetComponent<Transform>().position.x + backgrounds[backgrounds.Count - 1].GetComponent<SpriteRenderer>().bounds.size.x / 2;
+
+        // Check if moving right
+        if (lowerLeft.x > leftBGMinX)
         {
             GameObject leftBG = backgrounds[0];
             Vector3 temp = new Vector3(2 * leftBG.GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
             leftBG.GetComponent<Transform>().position += temp;
-
             backgrounds.RemoveAt(0);
             backgrounds.Add(leftBG);
         }
 
+        // Check if moving left
+        if (lowerRight.x < rightBGMaxX)
+        {
+            GameObject rightBG = backgrounds[backgrounds.Count - 1];
+            Vector3 temp = new Vector3(-2 * rightBG.GetComponent<SpriteRenderer>().bounds.size.x, 0, 0);
+            rightBG.GetComponent<Transform>().position += temp;
+            backgrounds.RemoveAt(backgrounds.Count - 1);
+            backgrounds.Insert(0, rightBG);
+        }
     }
 }
